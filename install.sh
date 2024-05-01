912f7f10200184cc89a291b62377991272a93783
Andres Aguirre <aguirregzz97@gmail.com>
2024-05-01 16:04:05 -0600

    fix

--- install.sh
+++ install.sh
@@ -8,18 +8,15 @@ rm ~/.zshrc
 # install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# change default terminal to zsh
-chsh -s $(which zsh)
-
 # install rbenv
 brew install rbenv ruby-build
 
 # install plugins
 brew install zsh-autosuggestions
 
-# source zshrc
-source ~/.zshrc
-
 # add git aliases
 git config --global alias.s status
 git config --gloabl alias.c commit
+ 
+# source zshrc
+source ~/.zshrc
