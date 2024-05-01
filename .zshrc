94c2dcdb7990209e391694ac056081d1c8d5911b
Andres Aguirre <aguirregzz97@gmail.com>
2024-05-01 13:15:47 -0600

    init

---
+++ .zshrc
@@ -0,0 +1,22 @@
+# add paths
+export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
+export PATH="/opt/homebrew/bin:$PATH"
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+
+
+# zsh plugins
+plugins=(
+  git
+  zsh-autosuggestions
+  web-search
+  copypath
+  copyfile
+  copybuffer
+)
+
+
+# disables spring (mainly for bugs related to rails)
+export DISABLE_SPRING=true
+
+source ~/.zshrc
---
+++ intall.sh
@@ -0,0 +1,17 @@
+# install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install zsh
+brew install zsh
+
+# install oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# change default terminal to zsh
+chsh -s $(which zsh)
+
+# install rbenv
+brew install rbenv ruby-build
+
+# install plugins
+brew install zsh-autosuggestions
