ad6127aed46fc24360f3fd71cbb6fdd267e43150
Andres Aguirre <aguirregzz97@gmail.com>
2024-05-01 16:39:27 -0600

    fix

--- install.sh
+++ install.sh
@@ -18,7 +18,31 @@ brew install zsh-autosuggestions
 git config --global alias.s status
 git config --gloabl alias.c commit
 
-mv .andres_zshrc .zshrc
- 
-# source zshrc
+echo '# add paths                                                                                                                                                                                      ✔ 
+export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
+export PATH="/opt/homebrew/bin:$PATH"
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+
+
+# oh my zsh
+export ZSH="$HOME/.oh-my-zsh"
+ZSH_THEME="robbyrussell"
+
+source $ZSH/oh-my-zsh.sh
+
+# zsh plugins
+plugins=(
+  git
+  zsh-autosuggestions
+  web-search
+  copypath
+  copyfile
+  copybuffer
+)
+
+
+# disables spring (mainly for bugs related to rails)
+export DISABLE_SPRING=true' > ~/.zshrc
+
 source ~/.zshrc
