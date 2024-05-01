55d2d46afd9d3adb6200717d97d70f4c327ae78e
Andres Aguirre <aguirregzz97@gmail.com>
2024-05-01 15:58:20 -0600

    fixes to zsh

--- .zshrc
+++ .zshrc
@@ -5,6 +5,10 @@ export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 
 
+# oh my zsh
+export ZSH="$HOME/.oh-my-zsh"
+ZSH_THEME="robbyrussell"
+
 # zsh plugins
 plugins=(
   git
--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 # install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# codespaces already comes with zsh but not ohmyzsh, remove and install ohmyzsh
+rm -rf ~/.oh-my-zsh
+rm ~/.zshrc
+
 # install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
