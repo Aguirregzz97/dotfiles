b84c3d9f293d037972c2aa5655a135e055001c09
Andres Aguirre <aguirregzz97@gmail.com>
2024-05-01 16:22:26 -0600

    fix

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ rm -rf ~/.oh-my-zsh
 rm ~/.zshrc
 
 # install oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
 
 # install rbenv
 brew install rbenv ruby-build
