08b5b293ab67610b26105a77a7e4ed55ffeca852
Andres Aguirre Gonzalez <aguirregzz97@gmail.com>
2024-10-08 10:39:03 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,15 +22,20 @@ git config --gloabl alias.c commit
 echo '# add paths                                                                                                                                                                                      ✔ 
 export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
 export PATH="/opt/homebrew/bin:$PATH"
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
 
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" 
+
+# Check if theres a ruby version file in the directory
+if [ -f .ruby-version ]; then
+    rvm use
+fi
 
 # oh my zsh
 export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME="robbyrussell"
-
-source $ZSH/oh-my-zsh.sh
 
 # zsh plugins
 plugins=(
@@ -43,6 +48,7 @@ plugins=(
   copybuffer
 )
 
+source $ZSH/oh-my-zsh.sh
 
 # disables spring (mainly for bugs related to rails)
 export DISABLE_SPRING=true' > ~/.zshrc
